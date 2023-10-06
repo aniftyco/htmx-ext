d432662bf76db4a5ffef28df087ca4fd6ef21123
Josh Manders <josh@joshmanders.com>
2023-10-06 10:10:20 -0500

    fix attribute updating

--- src/body-support.ts
+++ src/body-support.ts
@@ -3,14 +3,19 @@ type BodyAttribute = {
   value: string;
 };
 
-const parser = new DOMParser();
-
 htmx.defineExtension('body-support', {
   onEvent(name, event) {
     if (name === 'htmx:afterSwap') {
+      const parser = new DOMParser();
       const parsedResponse = parser.parseFromString(event.detail.xhr.response, 'text/html');
       const bodyAttributes = parsedResponse.body.attributes as unknown as BodyAttribute[];
 
+      // remove all attributes
+      while (event.detail.target.attributes.length > 0) {
+        event.detail.target.removeAttribute(event.detail.target.attributes[0].name);
+      }
+
+      // reset attributes based on response attributes
       for (const attribute of bodyAttributes) {
         event.detail.target.setAttribute(attribute.name, attribute.value);
       }
